958b2e6b6f004df2ae667987ad05d7051d81212f
nguyenvuongviet <nguyenvuongviet2k4@gmail.com>
2025-03-12 01:44:14 +0700

    thêm loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.tsx'
+import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
+import "./index.scss";
+import App from "./App.tsx";
+import store from "./store";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
